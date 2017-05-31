aa0027ef9c411c289bfc57c98071f37e5f5ca8f4
Sajeev Ramasamy <thorion3006@gmail.com>
2017-05-31 15:04:50 +0000

    User profile: completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,9 @@ class User < ApplicationRecord
       where(conditions.to_hash).first
     end
   end
+
+  def avatar_url(size)
+    gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :users, only: :show
   root 'welcome#index'
 
   if Rails.env.development?
